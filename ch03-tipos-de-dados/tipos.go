05de8e2be58bac052d92528838709c498bca771d
Daniel Wildt <dwildt@gmail.com>
2020-01-02 17:36:11 +0000

    adding more examples and type conversions

--- ch03-tipos-de-dados/tipos.go
+++ ch03-tipos-de-dados/tipos.go
@@ -6,6 +6,11 @@ func soma(a, b int) int {
     return a+b
 }
 
+func par(numero rune) bool {
+  return (numero%2 == 0)
+}
+
 func main() {
     fmt.Println(soma(4,5))
+    fmt.Println(par(4))
 }
--- ch03-tipos-de-dados/tipos_test.go
+++ ch03-tipos-de-dados/tipos_test.go
@@ -6,7 +6,16 @@ import (
 	"gotest.tools/assert"
 )
 
-func TestActive(t *testing.T) {
+func TestSoma(t *testing.T) {
         var x, y int = 8, 6
 	assert.Equal(t, soma(x,y), 14, "deveria ser 14")
 }
+
+func TestNumeroPar(t *testing.T) {
+    var p int32 = 25000
+    var i byte = 3
+
+    assert.Equal(t, par(p), true, "deveria ser true... 25000 é par")
+    assert.Equal(t, par(int32(i)), false,"deveria ser falso... 3 é impar")
+}
+
